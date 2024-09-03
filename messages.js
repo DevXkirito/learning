68f03db15f6d441e4a30a571ed3d6d4188bde3af
DevXkirito <111516421+DevXkirito@users.noreply.github.com>
2024-09-03 18:29:08 +0530

    Update and rename config.py to messages.js

---
+++ messages.js
@@ -0,0 +1,6 @@
+     module.exports = {
+       startMessage: '<b>Welcome to the Subtitle Burner Bot!</b> 🔥\n\n' +
+                    '<b>Get ready to burn subtitles into your videos!</b>\n\n',
+       imageLink: 'https://telegra.ph/file/216554d670ac648617b5c.jpg',
+       aboutMessage: 'This is a bot that helps you burn subtitles into your videos.' 
+     };
